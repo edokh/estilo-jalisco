32dd6c90999d55ef24e02257a0e5465d405a9853
edokh <edo.khudeeda@gmail.com>
2026-05-16 15:09:07 +0300

    guest layout edited

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
 
-<input @disabled($disabled) {{ $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm']) }}>
+<input @disabled($disabled) {{ $attributes->merge(['class' => 'border-gray-700 focus:border-indigo-200 focus:ring-indigo-200 rounded-md shadow-sm py-2']) }}>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -28,9 +28,9 @@
     </head>
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-            <div>
-                <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+            <div class="bg-black rounded-full h-[19rem] pt-16">
+                <a href="/" class="">
+                    <x-application-logo  />
                 </a>
             </div>
 
